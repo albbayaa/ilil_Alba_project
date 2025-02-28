f77df804d048452910ac564d26c5e9464e495308
Mouon <ahemsapsldk@gmail.com>
2025-02-28 17:48:18 +0900

    feat : cors

--- src/main/java/com/ilil/alba/config/CorsConfig.java
+++ src/main/java/com/ilil/alba/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package com.ilil.alba.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 경로에 대해
+                        .allowedOrigins("*") // 모든 도메인 허용
+                        .allowedMethods("*") // 모든 HTTP 메서드 허용
+                        .allowedHeaders("*") // 모든 헤더 허용
+                        .allowCredentials(false); // 인증 정보는 포함하지 않음
+            }
+        };
+    }
 }
+
